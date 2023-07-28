50a4b5fdcd470a299971019b78bebfcdad181fa4
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2023-07-28 14:41:19 +0400

    add

--- src/test/java/Homework4/TestExpandingTest.java
+++ src/test/java/Homework4/TestExpandingTest.java
@@ -1,15 +1,17 @@
 package Homework4;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.*;
 
 public class TestExpandingTest {
     WebDriver driver = new ChromeDriver();
-
-    //table[@id="table2"]//td[text()='jsmith@gmail.com']/../td//a[2]
-    //table[@id='table2']//td[@class="last-name"]
+    WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
     @Test
     public void testFindEmails() {
         driver.get("https://practice.expandtesting.com/tables");
@@ -18,6 +20,7 @@ public class TestExpandingTest {
             System.out.println(email.getText());
         }
         WebElement deleteEmail=driver.findElement(By.xpath(  "//table[@id=\"table2\"]//td[text()='jsmith@gmail.com']/../td//a[2]\n"));
+        driver.manage().window().maximize();
         deleteEmail.click();
 
     }
---
+++ src/test/java/Homework5/ForUTest.java
@@ -0,0 +1,64 @@
+package Homework5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class ForUTest {
+    private String actualName;
+    private String actualPrice;
+    private String expectedTitle;
+    private String expectedPrice;
+    private String totalPrice1;
+    WebDriver driver;
+    String Url;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        Url = "https://4u.am/en/";
+    }
+
+    @Test
+    public void testAddToBasket() throws InterruptedException {
+        driver.get(Url);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        WebElement realName = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//p//a[@href=\"/hy/product/zartucic-kupong\"]")));
+        WebElement realPrice = driver.findElement(By.xpath("//div[@id='offer1']//div[@class='swiper-slide'][1]//div[contains(@class, 'price_basket')]" +
+                "/p/span/span"));
+        WebElement basketElement = driver.findElement(By.xpath("//div[@id='offer1']//div[@class='swiper-slide'][1]//div[@class='item_basket']"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='offer1']//div[@class='swiper-slide'][1]//div[@class='item_basket']")));
+        basketElement.click();
+        WebElement toBasketElm = driver.findElement(By.xpath("//div[@id='basketIcon']"));
+        toBasketElm.click();
+        WebElement expectedTitle = driver.findElement(By.xpath("//div[@class='spinner_flex']//a//h4"));
+        WebElement totalPrice = driver.findElement(By.xpath("//span[@id = 'cartTotalPrice']"));
+        actualName = realName.getText();
+        actualPrice = realPrice.getText();
+        totalPrice1 = totalPrice.getText();
+    }
+    @Test(dataProvider = "real_ExpectedData")
+    public void checkOutComparisons(String actual,String expected){
+        Assert.assertEquals(actual,expected);
+    }
+    @DataProvider
+    public Object[][] real_ExpectedData() {
+        return new Object[][]{
+                {actualName, expectedTitle},
+                {actualPrice, expectedPrice},
+                {expectedPrice, totalPrice1}
+        };
+
+
+    }
+}
