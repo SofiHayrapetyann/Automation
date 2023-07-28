3887060b4cf666c66aa2fc104e08c1b58bd9f767
SofiHayrapetyann <sofi.hayrapetian@gmail.com>
2023-07-28 16:15:23 +0400

    Test 4U

--- src/test/java/Homework5/ForUTest.java
+++ src/test/java/Homework5/ForUTest.java
@@ -12,6 +12,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 
 public class ForUTest {
@@ -20,12 +21,13 @@ public class ForUTest {
     private String expectedTitle;
     private String expectedPrice;
     private String totalPrice1;
-    WebDriver driver;
+    private String expectedName1;
+    private String expectedPrice1;
+    WebDriver driver = new ChromeDriver();
     String Url;
 
     @BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver();
         Url = "https://4u.am/en/";
     }
 
@@ -33,32 +35,34 @@ public class ForUTest {
     public void testAddToBasket() throws InterruptedException {
         driver.get(Url);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        WebElement realName = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//p//a[@href=\"/hy/product/zartucic-kupong\"]")));
+        WebElement realName = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='swiper-slide']//div/p//a[@href='/hy/product/zartucic-kupong']")));
         WebElement realPrice = driver.findElement(By.xpath("//div[@id='offer1']//div[@class='swiper-slide'][1]//div[contains(@class, 'price_basket')]" +
                 "/p/span/span"));
         WebElement basketElement = driver.findElement(By.xpath("//div[@id='offer1']//div[@class='swiper-slide'][1]//div[@class='item_basket']"));
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='offer1']//div[@class='swiper-slide'][1]//div[@class='item_basket']")));
         basketElement.click();
         WebElement toBasketElm = driver.findElement(By.xpath("//div[@id='basketIcon']"));
         toBasketElm.click();
-        WebElement expectedTitle = driver.findElement(By.xpath("//div[@class='spinner_flex']//a//h4"));
+        WebElement expectedName = driver.findElement(By.xpath("//div[@class='spinner_flex']//a//h4"));
+        WebElement expectedPrice = driver.findElement(By.xpath("//p/span[@id='pricedfb6164402e24b9f0f795b242d26916d']"));
         WebElement totalPrice = driver.findElement(By.xpath("//span[@id = 'cartTotalPrice']"));
         actualName = realName.getText();
         actualPrice = realPrice.getText();
+        expectedName1 = expectedName.getText();
+        expectedPrice1 = expectedPrice.getText();
         totalPrice1 = totalPrice.getText();
     }
+
     @Test(dataProvider = "real_ExpectedData")
-    public void checkOutComparisons(String actual,String expected){
-        Assert.assertEquals(actual,expected);
+    public void checkOutComparisons(String actual, String expected) {
+        Assert.assertEquals(actual, expected);
     }
+
     @DataProvider
     public Object[][] real_ExpectedData() {
         return new Object[][]{
-                {actualName, expectedTitle},
-                {actualPrice, expectedPrice},
+                {actualName, expectedName1},
+                {actualPrice, expectedPrice1},
                 {expectedPrice, totalPrice1}
         };
-
-
     }
 }
